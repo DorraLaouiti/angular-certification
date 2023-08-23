2699ab0054a515a46a05a8d8a5de620d6cf4800b
Dorra <laouiti@viada.de>
2023-08-23 14:44:27 +0200

    Create routing for the quiz maker component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizMakerComponent } from './quiz-maker/quiz-maker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: QuizMakerComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
