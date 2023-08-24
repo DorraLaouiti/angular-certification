9c4f3b737bc9821d00729f6318761a926848abd7
Dorra <laouiti@viada.de>
2023-08-24 18:22:33 +0200

    add the service to get the list of questions along the answers

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -3,15 +3,19 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TriviaCategory } from '../models/triviaCategory';
 import { ResponseTriviaCtageories } from '../models/responseTriviaCategories';
+import { ResponseQuizQuestions } from '../models/responseQuizQuestions';
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
   baseUrl: string = 'https://opentdb.com';
-
   constructor(private http: HttpClient) {}
 
   getListTriviaCategories(): Observable<ResponseTriviaCtageories> {
     return this.http.get<ResponseTriviaCtageories>(`${this.baseUrl}/api_category.php`);
+  }
+
+  getListQuestions(categoryId: number, difficulty:string):Observable<ResponseQuizQuestions>{
+    return this.http.get<ResponseQuizQuestions>(`${this.baseUrl}/api.php?amount=5&category=${categoryId}&difficulty=${difficulty}&type=multiple`);
   }
 }
