04702e65795b30dfc22b1eb60a9e0fcc72d31661
Dorra <laouiti@viada.de>
2023-08-28 01:29:58 +0200

    Implment routing for results component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizMakerComponent } from './quiz-maker/quiz-maker.component';
+import { DisplayResultsComponent } from './display-results/display-results.component';
 
 const routes: Routes = [
   { path: "", component: QuizMakerComponent },
+  { path: "results", component: DisplayResultsComponent },
 
 ];
 
